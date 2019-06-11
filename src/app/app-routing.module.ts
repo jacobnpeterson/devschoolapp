148eb2b9f59ea911420e2ee74be724ba475da182
Jacob Peterson <jacobpeterson@JPETERSON-MBP-DAL.local>
2019-06-11 14:26:35 -0500

    working on routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SkillComponent } from './skill/skill.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'skill', component: SkillComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CardComponent } from './card/card.component';
+import { SkillComponent } from './skill/skill.component';
+import { PersonRowComponent } from './person-row/person-row.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    CardComponent
+    CardComponent,
+    SkillComponent,
+    PersonRowComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
