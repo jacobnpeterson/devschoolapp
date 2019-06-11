bc721bd3c0354b3610b7ab70f9de95f4f1241fc8
Chris Dixon <chrisdixon3295@gmail.com>
2019-06-11 14:20:07 -0500

    added profile, login, and endorse components w/ html

---
+++ src/app/endorse/endorse.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-endorse',
+  templateUrl: './endorse.component.html',
+  styleUrls: ['./endorse.component.less']
+})
+export class EndorseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
